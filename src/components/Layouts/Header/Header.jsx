7ae1fd038626488ac5fc7b70205571778925c656
sarchiandres <sarchiandres63@gmail.com>
2025-03-09 18:59:21 -0500

    se hace el responsive del header

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import { NavHerader } from '../../UI/NavHerader/NavHerader'
-import "./header.css"
+import React, { useState } from 'react';
+import { NavHerader } from '../../UI/NavHerader/NavHerader';
+import "./header.css";
+
 const logo = new URL('/src/assets/images/logomarca.jpeg', import.meta.url).href;
 
 export const Header = () => {
-  return (
-    <header>
-        <img src={logo} alt="logo" />
-        <NavHerader className="nav" />
-    </header>
-  );
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <header className="header">
+            <img src={logo} alt="logo" className="logo" />
+
+            {/* Botón hamburguesa */}
+            <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                ☰
+            </button>
+
+            <NavHerader classn={`nav ${menuOpen ? "open" : ""}`} navLink="navLink" />
+        </header>
+    );
 };
