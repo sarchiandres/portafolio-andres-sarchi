963094d19cd9a94a56e9eb3fce14b43689ea533d
sarchiandres <sarchiandres63@gmail.com>
2025-03-09 19:23:58 -0500

    se organiza el menu usando los estados

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'; 
 import { NavHerader } from '../../UI/NavHerader/NavHerader';
-import "./header.css";
-import logo from '../../../assets/images/logomarca.jpeg';  
-
+import logo from '../../../assets/images/logomarca.jpeg'
+import './Header.css';
 
 export const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false); 
+  const location = useLocation(); 
+  // Detecta el cambio de ruta y cierra el menú
+  useEffect(() => {
+    setMenuOpen(false); 
+  }, [location]); 
 
-    return (
-        <header className="header">
-            <img src={logo} alt="logo" className="logo" />
-
-            {/* Botón hamburguesa */}
-            <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-                ☰
-            </button>
-
-            <NavHerader classn={`nav ${menuOpen ? "open" : ""}`} navLink="navLink" />
-        </header>
-    );
+  return (
+    <header>
+      <img src={logo} alt="Logo" />
+      <button 
+        className="hamburger" 
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        ☰
+      </button>
+      <NavHerader classn={`nav ${menuOpen ? 'open' : ''}`} />
+    </header>
+  );
 };
