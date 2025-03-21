8fe2eee9c4dbced2bd31ac51d9168ad6de2672d2
sarchiandres <sarchiandres63@gmail.com>
2025-03-21 07:07:47 -0500

    se cambian palabras de el footer

--- src/components/Layouts/Footer/Footer.jsx
+++ src/components/Layouts/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css'
 export const Footer = ({fotter = 'fotter',contactcontainer1='contact-container1'}) => {
   return (
     <footer className={fotter}>
-    <div className='footerP'><p>&copy; {new Date().getFullYear()} Andrés Sarchi. Todos los derechos reservados.</p>
+    <div className='footerP'><p>&copy; {new Date().getFullYear()} Andrés Sarchi.</p>
     </div>
     <Netwoks contactcontainer={contactcontainer1}/>
     </footer>
